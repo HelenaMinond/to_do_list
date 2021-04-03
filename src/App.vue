e68762e919c0caba8e9866a8b031da4ad165db48
Helena Santana <helena.minond@gmail.com>
2021-04-03 17:56:12 -0300

    proyecto 'to do list'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Crea una nueva tarea</h1>
+
+    <section>
+      <label>Tarea</label>
+      <input
+        v-model="nuevaTarea"
+        type="text"
+        placeholder="Ingresa nueva tarea"
+        size="15"
+        maxlength="40"
+      />
+      <button @click="AgregarTarea()">Crear</button>
+    </section>
+
+    <h2>Lista</h2>
+
+    <ul>
+      <li v-for="(nuevaTarea, indice) in tareas" :key="(nuevaTarea, indice)">
+        {{ indice }}: {{ nuevaTarea }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tareas: ["Ir al supermercado", "Pagar internet", "Pagar Netflix"],
+      nuevaTarea: "",
+    };
+  },
+  methods: {
+    AgregarTarea() {
+      if (this.nuevaTarea === "") return;
+      this.tareas.push(this.nuevaTarea);
+      this.nuevaTarea = "";
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+div {
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1,
+h2 {
+  color: gray;
+  margin-top: 2rem;
+}
+
+label,
+li {
+  font-weight: 500;
+}
+
+label {
+  font-size: large;
+}
+
+input,
+button {
+  margin-left: 0.5rem;
+}
+
+button {
+  background-color: white;
+  border: solid 1px gray;
+  border-radius: 5px;
 }
 </style>
